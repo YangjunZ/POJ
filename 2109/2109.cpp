cade78f93c1355492a6840796c2fab7090e45bff
Yangjun <ThuYangjun>
2014-07-23 22:25:23 +0800

    poj2109 Accept
    1，poj 不支持 itoa，整数转换成字符串函数。
    2，创建的char[],要记得手动在末尾添加‘\0'。

--- 2109/2109.cpp
+++ 2109/2109.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 #include<String>
-#include<cmath>
-#include<cstdlib>
-
 
 using namespace std;
+string removeZeroFront(string str){
+	int pos = 0;
+	for( ; pos<str.size();pos++){
+		if(str[pos] != '0')
+			break;
+	}
+	return str.substr(pos);
+}
 
 string largeNumMultiply(string a, string b){
 	string product;
@@ -17,61 +22,113 @@ string largeNumMultiply(string a, string b){
 			int tmp = (a[ai]-'0' )*(b[bi]-'0');
 			product[ai+bi+1] += tmp % 10; 
 			product[ai+bi] += tmp/10; 
+			int ii = ai+bi;
+			while(ii>=0 && product[ii] - '0' > 9){
+				int xx = product[ii] - '0'	;
+				product[ii] = xx % 10 + '0';
+				ii = ii -1;
+				product[ii] += xx/10;
+			}
 		}
 	}
-	cout << product<<endl;
+
 	for(int pi=product.size()-1; pi>0; pi--){
 		int tmp = product[pi] - '0';
 		product[pi] = tmp % 10 + '0';
 		product[pi-1] += tmp/10;
 	}
-	int pos = 0;
-	for( ; pos<product.size();pos++){
-		if(product[pos] != '0')
-			break;
-	}
-	return product.substr(pos);
+
+	return removeZeroFront(product);
 }
 
 int largeNumCompare(string a, string b){
-	return a.compare(b);
+	a = removeZeroFront(a);
+	b = removeZeroFront(b);
+	if(a.size() < b.size()){
+		return -1;
+	}else if(a.size() > b.size()){
+		return 1;
+	}else{
+		return a.compare(b);
+	}
 }
 
 string largeNumPower(string a, int b){
 	//a^b
-	string res = '1';
-}
-
-
-
-double sqrtn(double n, double p){
-	double res = p;
-	cout << n << ','<<p<<endl;
-	while(n > 1){
-		res = sqrt(res);
-		n = n/2;
+	string res = "1";
+	string tmp = a;
+	while(b > 0){
+		if(b & 1 ){
+			res = largeNumMultiply(res,tmp);
+		}
+		tmp = largeNumMultiply(tmp, tmp);
+		b = b >> 1;
 	}
 	return res;
 }
 
-void systemPause(void){
-	cin.sync();
-    cout << "Please press any key to continue.... ";
-    cin.get();
+
+string integerToStr(int a){
+
+	#define CHLEN 12 //the resulet 1<=k<=10^9
+	char s[CHLEN];
+
+	for(int i=0; i<CHLEN; i++){
+		s[i] = '0';
+	}
+	s[CHLEN -1]= '\0';
+	int pos = CHLEN -2;
+	while( a > 0 && pos >= 0){
+		s[pos] = a%10 + '0';
+		a = a/10;
+		pos --;
+	}
+
+	string str = s;
+	str = removeZeroFront(str);
+	return str;
 }
+
+
+// void systemPause(void){
+// 	cin.sync();
+//     cout << "Please press any key to continue.... ";
+//     cin.get();
+// }
 
 
 int main()
 {
 	
-	string n, p;
-	while(true){
-		cin >> n >> p;
-		cout << n << '#'<<p<<endl;
-		// system("PAUSE");
-		cout << largeNumCompare(n,p) <<endl;
-		cout << largeNumMultiply(n,p) <<endl;
+	string  p;
+	int n;
+	while(cin >> n >> p){
+		int klen = p.size()/n;
+		if( klen * n < p.size()) {
+			klen = klen + 1;
+		}
+		int min =  1;
+		int tmp = klen;
+		while( -- tmp ){
+			min = min * 10;
+		}
+		int max = min * 10;
+		int mid = (min + max)/2;
+		while( true){
+			string pres = largeNumPower( integerToStr(mid), n);
+			int cres = largeNumCompare(pres, p);
+			if( cres < 0){// res < p
+				min = mid + 1;
+			}else if(cres >0){// res > p
+				max = mid - 1;
+			}else{
+				break;
+			}
+			if (min > max ) break;
+			mid = (min + max)/2;
+		}
+		cout << mid<<endl;
 	}
-	systemPause();
+	// systemPause();
 	return 0;
 }
--- 2109/README.md
+++ 2109/README.md
@@ -1,25 +1,25 @@
-Poj2109
----------
-
-Description
-
-Current work in cryptography involves (among other things) large prime numbers and computing powers of numbers among these primes. Work in this area has resulted in the practical use of results from number theory and other branches of mathematics once considered to be only of theoretical interest. 
-This problem involves the efficient computation of integer roots of numbers. 
-Given an integer n>=1 and an integer p>= 1 you have to write a program that determines the n th positive root of p. In this problem, given such integers n and p, p will always be of the form k to the nth. power, for an integer k (this integer is what your program must find).
-Input
-
-The input consists of a sequence of integer pairs n and p with each integer on a line by itself. For all such pairs 1<=n<= 200, 1<=p<10101 and there exists an integer k, 1<=k<=109 such that kn = p.
-Output
-
-For each integer pair n and p the value k should be printed, i.e., the number k such that k n =p.
-Sample Input
-
-2 16
-3 27
-7 4357186184021382204544
-Sample Output
-
-4
-3
-1234
-
+Poj2109<br>
+---------<br>
+<br>
+Description<br>
+<br>
+Current work in cryptography involves (among other things) large prime numbers and computing powers of numbers among these primes. Work in this area has resulted in the practical use of results from number theory and other branches of mathematics once considered to be only of theoretical interest. <br>
+This problem involves the efficient computation of integer roots of numbers. <br>
+Given an integer n>=1 and an integer p>= 1 you have to write a program that determines the n th positive root of p. In this problem, given such integers n and p, p will always be of the form k to the nth. power, for an integer k (this integer is what your program must find).<br>
+Input<br>
+<br>
+The input consists of a sequence of integer pairs n and p with each integer on a line by itself. For all such pairs 1<=n<= 200, 1<=p<10101 and there exists an integer k, 1<=k<=109 such that kn = p.<br>
+Output<br>
+<br>
+For each integer pair n and p the value k should be printed, i.e., the number k such that k n =p.<br>
+Sample Input<br>
+<br>
+2 16<br>
+3 27<br>
+7 4357186184021382204544<br>
+Sample Output<br>
+<br>
+4<br>
+3<br>
+1234<br>
+<br>
