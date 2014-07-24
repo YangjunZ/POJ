dced08edb29b7e696a4cd801b85e13bfb7ed0089
Yangjun <ThuYangjun@gmail.com>
2014-07-24 20:58:31 +0800

    POJ2586，AC
    解法很简单，就是使用最少的deficit月份，令8个连续时间段1月~5月，2月~6月,...,8月~12月
    的报表为负。为获取最大可能收益，设连续5个月份中至少需要i个deficit月份，如果i<=3，则deficit月份数为2i。如果i>3，则deficit月份数为2i+i-3。deficit月份均尽量排在连续的5月份末尾。

--- 2586/2586.cpp
+++ 2586/2586.cpp
@@ -2,23 +2,32 @@
 
 using namespace std;
 
-void systemPause(void){
-	cin.sync();
-	cout << "press any key to continue..."<<endl;
-	cin.get();
+void systemPause(void)
+{
+    cin.sync();
+    cout << "press any key to continue..." << endl;
+    cin.get();
 }
 
 int main()
 {
-	cout << " hello world"<<endl;
-	long long s, d;
-	while(cin >>s>>d){
-		if(s*10 >= d*2 ){
-			cout << s*10 -d*2<<endl;
-		}else{
-			cout<<"Deficit"<<endl;
-		}
-	}
-	systemPause();
-	return 0;
+
+    long long  s, d;
+    while (cin >> s >> d) {
+        int xx = 0;
+        int total_surplus = 0;
+        while ( xx <= 5 && xx * d <= (5 - xx)*s ) xx++;
+        if ( xx <= 3) {
+            total_surplus = (12 - 2 * xx) * s - 2 * xx * d ;
+        } else {
+            total_surplus = (12 - (2 * xx + xx - 3)) * s - (2 * xx + xx - 3) * d;
+        }
+        if (total_surplus >= 0) {
+            cout << total_surplus << endl;
+        } else {
+            cout << "Deficit" << endl;
+        }
+    }
+    // systemPause();
+    return 0;
 }
